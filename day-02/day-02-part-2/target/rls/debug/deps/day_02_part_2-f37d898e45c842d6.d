f1e5a5a2087823c040a9f6ff4bf28889ffc915d7
Michael Conrad <mmitton@gmail.com>
2021-12-02 19:36:57 -0500

    Day 2

---
+++ day-02/day-02-part-2/target/rls/debug/deps/day_02_part_2-f37d898e45c842d6.d
@@ -0,0 +1,5 @@
+/home/mmitton/src/advent/2020/day-02/day-02-part-2/target/rls/debug/deps/day_02_part_2-f37d898e45c842d6.rmeta: day-02/day-02-part-2/src/main.rs
+
+/home/mmitton/src/advent/2020/day-02/day-02-part-2/target/rls/debug/deps/day_02_part_2-f37d898e45c842d6.d: day-02/day-02-part-2/src/main.rs
+
+day-02/day-02-part-2/src/main.rs:
