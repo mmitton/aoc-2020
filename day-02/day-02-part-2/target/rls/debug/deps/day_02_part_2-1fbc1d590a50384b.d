b78de159432a0af4d5a3d0b051ba67890d8811c8
Michael Conrad <mmitton@gmail.com>
2021-12-02 19:38:15 -0500

    Remove target

--- day-02/day-02-part-2/target/rls/debug/deps/day_02_part_2-1fbc1d590a50384b.d
+++
@@ -1,5 +0,0 @@
-/home/mmitton/src/advent/2020/day-02/day-02-part-2/target/rls/debug/deps/day_02_part_2-1fbc1d590a50384b.rmeta: day-02/day-02-part-2/src/main.rs
-
-/home/mmitton/src/advent/2020/day-02/day-02-part-2/target/rls/debug/deps/day_02_part_2-1fbc1d590a50384b.d: day-02/day-02-part-2/src/main.rs
-
-day-02/day-02-part-2/src/main.rs:
